2bed59dea6f689ec029c55274d553e1a55b4329b
Replit user <>
2022-12-08 01:30:44 +0000

    Finish the algorithm code for solving an expression that follows the order of operations

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let firstNum = 0; secondNum = 0, equalsPressed = false;
 
 for (i = 0; i < buttonElements.length; i++) {
   buttonElements[i].onclick = function() {
-    if (equalsPressed === true) {
+    if (equalsPressed === true && this.className != "operator") {
       clear();
     }
     screen.innerText += document.getElementById(this.id).innerText;
@@ -26,138 +26,82 @@ clearBtn.onclick = function() {
   clear()
 }
 
-negativeBtn.onclick = function() {
+negativeBtn.onclick = function() {//fix this function
   if (screen.innerText != 0) {
     screen.innerText = (-1) * parseInt(screen.innerText);
   }
 }
-/*
-function getNum(button) {
-  equalsPressed = false;
-  firstNum = parseFloat(screen.innerText);
-  screen.innerText += document.getElementById(button.id).innerText;
-}
-
-addBtn.onclick = function() {
-  getNum(addBtn);
-}
-
-subtractBtn.onclick = function() {
-  getNum(subtractBtn);
-}
-
-multiplyBtn.onclick = function() {
-  getNum(multiplyBtn);
-}
-
-divideBtn.onclick = function() {
-  getNum(divideBtn);
-}
-
-function scanExpression(position, expression) {
-  let secondNumString = "";
-  for (j = position + 1; j < expression.length; j++) {
-    secondNumString += expression.charAt(j);
-  }
-  secondNum = parseFloat(secondNumString);
-}
-
-function add(position, expression) {
-  scanExpression(position, expression);
-  screen.innerText = secondNum + firstNum;
-}
-
-function subtract(position, expression) {
-  scanExpression(position, expression)
-  screen.innerText = firstNum - secondNum;
-}
-
-function multiply(position, expression) {
-  scanExpression(position, expression)
-  screen.innerText = firstNum * secondNum;
-}
-
-function divide(position, expression) {
-  scanExpression(position, expression)
-  screen.innerText = firstNum / secondNum;
-}*/
 
 equalsBtn.onclick = function() {
   let expression = screen.innerText;
   screen.innerText = operate(expression);
-  /*for (i = expression.length - 1; i >= 0; i--) {
-    if(isNaN(expression.charAt(expression.length-1))){
-      screen.innerText = "Error!";
-      break;
-    }
-    else if (expression.charAt(i) === "+") {
-      add(i, expression);
-      break;
-    }
-    else if (expression.charAt(i) === "-" && i != 0) {
-      subtract(i, expression);
-      break;
-    }
-    else if (expression.charAt(i) === "x" && i != 0) {
-      multiply(i, expression);
-      break;
-    }
-    else if (expression.charAt(i) === "÷" && i != 0) {
-      divide(i, expression);
-      break;
-    }
-  }
-  equalsPressed = true;*/
+  equalsPressed = true;
 }
+
 function scanFirstNum(position, expression) {
-  for (i = position - 1; i >= 0; i--) {
-    let char = expression.charAt(i);
-    if (char === "+" || char === "-" || char === "x" || char === "÷") {
-      return parseFloat(expression.slice(i + 1, position))
-    }
-    else {
-      return parseFloat(expression.slice(i, position));
+  for (j = position - 1; j >= 0; j--) {
+    let char = expression.charAt(j);
+    if (j != 0 && (char === "+" || char === "-" || char === "x" || char === "÷")) {
+      return parseFloat(expression.slice(j + 1, position));
     }
   }
+  return parseFloat(expression.slice(j + 1, position));
 }
 
 function scanSecondNum(position, expression) {
-  for (i = position + 1; i < expression.length; i--) {
-    let char = expression.charAt(i);
+  for (k = position + 1; k < expression.length; k++) {
+    let char = expression.charAt(k);
     if (char === "+" || char === "-" || char === "x" || char === "÷") {
-      return parseFloat(expression.slice(position + 1, i))
-    }
-    else {
-      return parseFloat(expression.slice(position + 1, expression.length));
+      return parseFloat(expression.slice(position + 1, k))
     }
   }
+  return parseFloat(expression.slice(position + 1, expression.length));
 }
 
 function operate(expression) {
-  let secondNum = 0, firstNum = 0, newNum = 0, hasOperator = true;
-  for (i = expression.length - 1; i >= 0; i--) {
-    let char = expression.charAt(i);
+  let secondNum = 0, firstNum = 0, ans = 0, hasOperator = true;
+  for (position = 0; position < expression.length; position++) {
+    let char = expression.charAt(position);
     if (char === "x" || char === "÷") {
       hasOperator = true;
-      firstNum = scanFirstNum(i, expression);
-      secondNum = scanSecondNum(i, expression);
+      firstNum = scanFirstNum(position, expression);
+      secondNum = scanSecondNum(position, expression);
       if (char === "x") {
-        newNum = secondNum * firstNum;
+        ans = firstNum * secondNum;
       }
       else if (char === "÷") {
-        newNum = firstNum / secondNum;
+        if (secondNum === 0) {
+          return "Can't divide by zero bruh";
+        }
+        ans = firstNum / secondNum;
       }
-      break;
+      expression = expression.substring(0, j + 1) + ans + expression.substring(k, expression.length);
+      position = -1;
     }
-
     else {
       hasOperator = false;
     }
   }
-  if (hasOperator === false) {
-    secondNum = expression;
+  for (position = 0; position < expression.length; position++) {
+    let char = expression.charAt(position);
+    if (position != 0 && (char === "+" || char === "-")) {
+      hasOperator = true;
+      firstNum = scanFirstNum(position, expression);
+      secondNum = scanSecondNum(position, expression);
+      if (char === "+") {
+        ans = firstNum + secondNum;
+      }
+      else if (char === "-") {
+        ans = firstNum - secondNum;
+      }
+      expression = expression.substring(0, j + 1) + ans + expression.substring(k, expression.length);
+      position = -1;
+    }
+    else {
+      hasOperator = false;
+    }
   }
-  return newNum;
+  return ans;
 }
 
 
