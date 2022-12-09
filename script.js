087203547b59926211eed836e68937adc17818b7
Replit user <>
2022-12-09 01:22:10 +0000

    Fix decimal being used multiple times in the same number

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 const buttonElements = document.querySelectorAll("button"), zeroBtn = document.getElementById("zero"),
   screen = document.getElementById("screen"), clearBtn = document.getElementById("clearBtn"),
-  negativeBtn = document.getElementById("negative"), equalsBtn = document.getElementById("equals"),
-  addBtn = document.getElementById("add"), subtractBtn = document.getElementById("subtract"),
-  multiplyBtn = document.getElementById("multiply"), divideBtn = document.getElementById("divide");
+  negativeBtn = document.getElementById("negative"), equalsBtn = document.getElementById("equals"), dotBtn = document.getElementById("dot");
 
-let firstNum = 0; secondNum = 0, equalsPressed = false;
+let equalsPressed = false, dotPressed = false;
 
 for (i = 0; i < buttonElements.length; i++) {
   buttonElements[i].onclick = function() {
     if (equalsPressed === true && this.className != "operator") {
       clear();
     }
-    screen.innerText += document.getElementById(this.id).innerText;
-    equalsPressed = false;
+    if (this.id === "dot") {
+      if (dotPressed === true) {
+        screen.innerText += "";
+      }
+      else {
+        screen.innerText += document.getElementById(this.id).innerText;
+        dotPressed = true;
+      }
+    }
+    else {
+      screen.innerText += document.getElementById(this.id).innerText;
+      equalsPressed = false;
+      if (this.className === "operator") {
+        dotBtn.setAttribute("id", "dot");
+        dotPressed = false;
+      }
+    }
   }
 }
 
 function clear() {
   screen.innerText = null;
-  firstNum = 0;
-  secondNum = 0;
 }
-
 clearBtn.onclick = function() {
   clear()
 }
@@ -59,49 +69,50 @@ function scanSecondNum(position, expression) {
 }
 
 function operate(expression) {
-  let secondNum = 0, firstNum = 0, ans = 0, hasOperator = true;
-  for (position = 0; position < expression.length; position++) {
+  let secondNum = 0, firstNum = 0, result = 0, hasOperator = true;
+  expression = expression.trim();
+  for (position = 0; position < expression.length; position++) { //first for loop searches the expression for multiplication or division
     let char = expression.charAt(position);
     if (char === "x" || char === "÷") {
       hasOperator = true;
       firstNum = scanFirstNum(position, expression);
       secondNum = scanSecondNum(position, expression);
       if (char === "x") {
-        ans = firstNum * secondNum;
+        result = firstNum * secondNum;
       }
       else if (char === "÷") {
         if (secondNum === 0) {
           return "Can't divide by zero bruh";
         }
-        ans = firstNum / secondNum;
+        result = firstNum / secondNum;
       }
-      expression = expression.substring(0, j + 1) + ans + expression.substring(k, expression.length);
+      expression = expression.substring(0, j + 1) + result + expression.substring(k, expression.length);
       position = -1;
     }
     else {
       hasOperator = false;
     }
   }
-  for (position = 0; position < expression.length; position++) {
+  for (position = 0; position < expression.length; position++) {//second for loop searches the expression for addition or subtraction
     let char = expression.charAt(position);
     if (position != 0 && (char === "+" || char === "-")) {
       hasOperator = true;
       firstNum = scanFirstNum(position, expression);
       secondNum = scanSecondNum(position, expression);
       if (char === "+") {
-        ans = firstNum + secondNum;
+        result = firstNum + secondNum;
       }
       else if (char === "-") {
-        ans = firstNum - secondNum;
+        result = firstNum - secondNum;
       }
-      expression = expression.substring(0, j + 1) + ans + expression.substring(k, expression.length);
+      expression = expression.substring(0, j + 1) + result + expression.substring(k, expression.length);
       position = -1;
     }
     else {
       hasOperator = false;
     }
   }
-  return ans;
+  return result;
 }
 
 
